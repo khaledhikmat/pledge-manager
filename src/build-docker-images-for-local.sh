41d0feae087e514ee2b6fe585c7233493d926c2f
Khaled Aboudan <khaled@KhaledsMBPMain.attlocal.net>
2022-07-10 10:09:01 -0500

    Fixed Docker tag version

--- src/build-docker-images-for-local.sh
+++ src/build-docker-images-for-local.sh
@@ -1,4 +1,4 @@
-docker image build -t pledgemanager-be-campaigns:1.5 . -f Dockerfile-backend-campaigns
-docker image build -t pledgemanager-be-users:1.5 . -f Dockerfile-backend-users
-docker image build -t pledgemanager-fe-functions:1.5 . -f Dockerfile-frontend-functions
-docker image build -t pledgemanager-fe-client:1.5 . -f Dockerfile-frontend-client
+docker image build -t pledgemanager-be-campaigns:latest . -f Dockerfile-backend-campaigns
+docker image build -t pledgemanager-be-users:latest . -f Dockerfile-backend-users
+docker image build -t pledgemanager-fe-functions:latest . -f Dockerfile-frontend-functions
+docker image build -t pledgemanager-fe-client:latest . -f Dockerfile-frontend-client
